26b4030f6af5ceade03fdffeddb2686806fc16cc
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-03-09 19:58:48 +0300

    Refactoring views.py

--- src/views.py
+++ src/views.py
@@ -2,16 +2,36 @@ from django.shortcuts import render
 
 
 def loading(request):
+    """
+    Loading page.
+    :param request:
+    :return:
+    """
     return render(request, "loading.html")
 
 
 def main(request):
+    """
+        Main page.
+        :param request:
+        :return:
+        """
     return render(request, "main_page.html")
 
 
 def catalog(request):
+    """
+        Catalog
+        :param request:
+        :return:
+        """
     return render(request, "catalog.html")
 
 
 def animals_catalog(request):
+    """
+        Animals catalog.
+        :param request:
+        :return:
+        """
     return render(request, "animals-catalog.html")
