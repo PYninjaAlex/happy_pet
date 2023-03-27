187999be5e099b3590087f135a1b06cc5a3420c2
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-03-27 12:19:14 +0300

    New tests added.

--- src/tests.py
+++ src/tests.py
@@ -1,5 +1,43 @@
 from django.test import TestCase
 
+
 class ViewsTests(TestCase):
-    def test_true(self):
-        self.assertTrue(True)
+    def test_loading(self):
+        response = self.client.get("")
+        self.assertEqual(response.status_code, 200)
+
+    def test_main(self):
+        response = self.client.get("/main-page")
+        self.assertEqual(response.status_code, 200)
+
+    def test_catalog(self):
+        response = self.client.get("/main-page/catalog")
+        self.assertEqual(response.status_code, 200)
+
+    def test_animals_catalog(self):
+        response = self.client.get("/main-page/animals-catalog")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_service(self):
+        response = self.client.get("/main-page/about-service")
+        self.assertEqual(response.status_code, 200)
+
+    def test_cats(self):
+        response = self.client.get("/main-page/animals-catalog/cats")
+        self.assertEqual(response.status_code, 200)
+
+    def test_dogs(self):
+        response = self.client.get("/main-page/animals-catalog/dogs")
+        self.assertEqual(response.status_code, 200)
+
+    def test_parrots(self):
+        response = self.client.get("/main-page/animals-catalog/parrots")
+        self.assertEqual(response.status_code, 200)
+
+    def test_hamsters(self):
+        response = self.client.get("/main-page/animals-catalog/hamsters")
+        self.assertEqual(response.status_code, 200)
+
+    def test_orders(self):
+        response = self.client.get("/main-page/animals-catalog/orders")
+        self.assertEqual(response.status_code, 200)
